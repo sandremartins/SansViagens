0d8e580a7d6e7da87f84c66a300ae560bc558b36
Alessandre Martins <sandremartins@gmail.com>
2022-02-14 12:02:08 -0400

    Commit inicial

---
+++ SansViagens/SansViagens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SansViagens
+//
+//  Created by Alessandre Martins on 2/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
